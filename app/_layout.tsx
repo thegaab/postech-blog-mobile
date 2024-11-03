9428a3402a72a16331a5612984fa3967f6ca9076
Carolina Moutella <emaildacarolmoutella@gmail.com>
2024-11-03 15:09:07 -0300

    config: NativeWind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,7 @@
 import { Stack } from "expo-router";
 
+import "../constants/global.css";
+
 export default function RootLayout() {
   return (
     <Stack>
--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,10 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex justify-center items-center bg-blue-600">
+      <Text className="text-white text-lg">
+        Edit app/index.tsx to edit this screen.
+      </Text>
     </View>
   );
 }
