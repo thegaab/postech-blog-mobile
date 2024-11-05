7de4796b7fe75793c186af629a029c1b419a27ff
Carolina Moutella <emaildacarolmoutella@gmail.com>
2024-11-05 20:03:34 -0300

    feat: post view

---
+++ app/screens/Post/index.tsx
@@ -0,0 +1,10 @@
+import { RootStackParamList } from "@/routes/app.routes";
+import { PostScreen } from "@/ui/screens/Post";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+
+type Props = NativeStackScreenProps<RootStackParamList, "post">;
+
+export default function Post({ route }: Props) {
+  const { postId } = route.params;
+  return <PostScreen postId={postId} />;
+}
--- ui/navigation/index.ts
+++ ui/navigation/index.ts
@@ -3,7 +3,7 @@ import { ParamListBase, useNavigation } from "@react-navigation/native";
 export const useNavigate = () => {
   const navigation = useNavigation();
 
-  function goToPage(screenName: string, options?: ParamListBase) {
+  function goToPage(screenName: string, options?: Record<string, any>) {
     const params = [screenName, options] as unknown as never;
     return navigation.navigate(...params);
   }
