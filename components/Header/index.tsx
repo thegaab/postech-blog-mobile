ab9319e6dda30443a9c03a278eaf80666a0fe1c2
Carolina Moutella <emaildacarolmoutella@gmail.com>
2024-11-05 20:10:00 -0300

    patch: return home on press blog name

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -9,7 +9,11 @@ const Header = () => {
   return (
     <Box className="flex justify-center items-center h-16 bg-stone-800 px-2 w-full">
       <Box className="flex flex-row justify-between items-center w-full">
-        <Heading size="md" color="primary.600">
+        <Heading
+          size="md"
+          color="primary.600"
+          onPress={() => navigate.to("home")}
+        >
           PosTech
         </Heading>
         {isLogged ? (
