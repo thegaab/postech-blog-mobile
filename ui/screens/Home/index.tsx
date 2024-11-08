b14329ae5564492aabe2a1964bd23af8d37a9d10
Carolina Moutella <emaildacarolmoutella@gmail.com>
2024-11-07 21:15:13 -0300

    fix: home add search bar

--- ui/screens/Home/index.tsx
+++ ui/screens/Home/index.tsx
@@ -1,33 +1,46 @@
 import { Box, View } from "native-base";
 import BaseTemplate from "@/ui/templates/BaseTemplate";
 import { useEffect, useState } from "react";
-import { InterfaceList } from "@/types/apiPatterns";
 import { PostInterface } from "@/types";
 import getPublicPosts from "@/api/getPosts";
 import PublicPostPreview from "@/components/PublicPostPreview";
 import List from "@/components/List";
+import SearchBar from "@/components/SearchBar";
 
+// todo: handle keyword
 export function HomeScreen() {
   const [posts, setPosts] = useState<PostInterface[]>([]);
+  const [keyword, setKeyword] = useState<string>("");
   const [page, setPage] = useState(1);
   const [maxPages, setMaxPages] = useState(1);
 
   const requestPosts = getPublicPosts(page);
 
-  const getPosts = async () => {
-    const listData: InterfaceList<PostInterface> = await requestPosts.submit();
+  const handleKeyword = (t: string) => {
+    setKeyword(t);
+    setPage(1);
+  };
 
-    setPosts([...posts, ...listData.data]);
-    setMaxPages(Math.ceil(listData.totalItems / listData.itemsPerPage));
+  const handleSubmit = async () => {
+    const data = await requestPosts.submit();
+
+    if (!data || !data.data) return;
+
+    const updateList = posts.length >= 0 ? data.data : [...posts, ...data.data];
+    setPosts(updateList);
+    setMaxPages(Math.ceil(data.totalItems / data.itemsPerPage));
   };
 
   useEffect(() => {
-    getPosts();
+    handleSubmit();
   }, [page]);
 
   return (
     <BaseTemplate>
       <View className="pt-8">
+        <Box className="w-full mb-6">
+          <SearchBar onSearch={handleKeyword} />
+        </Box>
         <Box className="w-full p-2 mb-6">
           <List
             items={posts}
